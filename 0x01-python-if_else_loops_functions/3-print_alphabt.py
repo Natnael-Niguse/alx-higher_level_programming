7d0a677a47b490f43f4eb10346b6d1c33b200e2f
MNatiki <natiart07@gmail.com>
2023-06-06 10:05:15 -0700

    python2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+output = ""
 for char in range(ord('a'), ord('z') + 1):
     if chr(char) not in 'qe':
-        print(chr(char), end='')
+        output += chr(char)
+print("{}".format(output), end='')
+
