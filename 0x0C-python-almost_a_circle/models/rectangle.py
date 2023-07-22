bf7317e14d6fbe506dd79480f636531ff4faf9b2
MNatiki <natiart07@gmail.com>
2023-07-22 03:00:42 -0700

    python

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,8 +77,11 @@ class Rectangle(Base):
 
     def display(self):
         """Print the rectangle using '#' characters"""
+        for _ in range(self.__y):
+            print()
+
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print("" * self.__x + "#" * self.__width)
 
     def __str__(self):
 
