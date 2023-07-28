828d854835dfc9ce83ccfe109b38e3726e688aa3
MNatiki <natiart07@gmail.com>
2023-07-28 00:45:55 -0700

    class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -122,3 +122,13 @@ class Rectangle(Base):
                     self.x = value
                 elif key == 'y':
                     self.y = value
+
+    def to_dictionary(self):
+        """Returns the dictionary of a Rectangle object"""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
