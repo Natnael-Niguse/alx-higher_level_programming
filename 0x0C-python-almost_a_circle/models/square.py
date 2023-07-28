a64d9d2d29e35487a8d2e74a830dba7818bd7fb0
MNatiki <natiart07@gmail.com>
2023-07-28 00:14:43 -0700

    class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module contains the square class"""
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """This is a class inherited from the rectangle class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes the Square class"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Getter for the size attribute"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size attribute"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """String representation of the Square instance"""
+        return f"[square] ({self.id}) {self.x}/{self.y} - {self.size}"
