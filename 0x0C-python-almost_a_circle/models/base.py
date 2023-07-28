6412772cb98d39a669911d71fd321cfb60775c14
MNatiki <natiart07@gmail.com>
2023-07-28 01:05:01 -0700

    class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -66,3 +66,38 @@ class Base:
                 return [cls.create(**data) for data in list_dicts]
         except FileNotFoundError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serializes instances to CSV format and writes to a file"""
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".csv"
+        with open(filename, 'w', newline='') as file:
+            writer = csv.writer(file)
+            for obj in list_objs:
+                writer.writerow(obj.to_csv_row())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserializes instances from CSV format"""
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, 'r', newline='') as file:
+                reader = csv.reader(file)
+                instances = []
+                for row in reader:
+                    instance = cls.from_csv_row(row)
+                    instances.append(instance)
+                return instances
+        except FileNotFoundError:
+            return []
+
+    def to_csv_row(self):
+        """Returns a CSV row representation of the instance"""
+        raise NotImplementedError("to_csv_row method must be implemented in subclasses")
+
+    @classmethod
+    def from_csv_row(cls, row):
+        """Creates an instance from a CSV row"""
+        raise NotImplementedError("from_csv_row method must be implemented in subclasses")
