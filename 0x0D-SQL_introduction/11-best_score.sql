dbf962c1a81be941464a6ac5491489b95c54dd3e
MNatiki <natiart07@gmail.com>
2023-08-15 10:52:08 -0700

    sql

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- Retrieve scores and names from "second_table" where score is greater than or equal to 10,
+-- and order the results by score in descending order
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+
