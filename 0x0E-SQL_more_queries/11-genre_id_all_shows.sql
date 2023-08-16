53bc9d3f7fc34e1149789889f3212f675b7b8aec
MNatiki <natiart07@gmail.com>
2023-08-16 14:29:40 -0700

    sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- Select the 'title' column from the 'tv_shows' table,
+-- and the 'genre_id' column from the 'tv_show_genres' table
+-- using a left join based on the 'id' and 'show_id' columns, respectively
+-- and order the results first by the 'title' column of the 'tv_shows' table
+-- and then by the 'genre_id' column of the 'tv_show_genres' table
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
+
