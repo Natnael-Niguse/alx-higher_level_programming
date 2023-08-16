dbe0317d9f6c5d3ef89684b0b4e3145e70b19dd1
MNatiki <natiart07@gmail.com>
2023-08-16 14:18:33 -0700

    sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- Displaying privileges for specific users on the local host.
+-- lists all privileges of MySQL pakati pa user2 user_0d_1 na user_0d_2
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
