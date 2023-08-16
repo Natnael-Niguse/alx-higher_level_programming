4469ca93b63a736e7beeaa2c9de2599d60b1b064
MNatiki <natiart07@gmail.com>
2023-08-16 14:38:53 -0700

    sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,3 +1,8 @@
+-- Select the title of TV shows and their corresponding rating sum
+-- by joining the 'tv_shows' and 'tv_show_ratings' tables on the 'id' and 'show_id' columns
+-- Group the results by show title and calculate the sum of ratings for each show
+-- Order the results in descending order based on the rating sum
+
 SELECT tv_shows.title, SUM(rating) AS rating_sum
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
