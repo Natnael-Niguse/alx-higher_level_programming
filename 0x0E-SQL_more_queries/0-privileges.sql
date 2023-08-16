1f1a68160c046bd1f49de85e3c9fc2f4f2379156
MNatiki <natiart07@gmail.com>
2023-08-16 13:24:16 -0700

    sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Displaying privileges for specific users on the local host.
+-- This command retrieves the grants for 'user_0d_1' and 'user_0d_2' accounts.
+-- It provides information about the permissions and privileges these users have within the database environment.
+-- Useful for auditing and managing user access to the database.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
